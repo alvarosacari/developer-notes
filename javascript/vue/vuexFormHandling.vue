5250ac39d1a413d1e55e017a4f1fadc72ba2b77d
Alvaro Sacari <alvaro.sacari@gmail.com>
2022-04-30 20:17:20 -0500

    feat: add vuexFormHandling vue file

---
+++ javascript/vue/vuexFormHandling.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  computed: {
+    message: {
+      get() {
+        return this.$store.state.obj.message;
+      },
+      set(value) {
+        this.$store.commit("updateMessage", value);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
